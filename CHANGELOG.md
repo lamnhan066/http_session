a476e9bf495e174ce293e3732f9da5c9ad0af5c2
Vursin <contact@vursin.com>
2022-07-04 13:14:56 +0700

    Export Response, Request from http package.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2
+
+* Export Response, Request from http package.
+
 ## 0.0.1
 
 * Initial Release
--- lib/http_session.dart
+++ lib/http_session.dart
@@ -1,3 +1,5 @@
 library http_session;
 
+export 'package:http/http.dart' show Response, Request;
+
 export 'src/http_session.dart';
