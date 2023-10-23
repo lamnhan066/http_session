f348924c6bc4b99b0e41718811c1dbf116f007f8
Ege Feyzioglu <egefeyzioglu@yahoo.com>
2023-10-22 23:13:58 -0400

    Implemented _domainCompare and _toCanonical*
    *: Also created a yet to be implemented _toPunyCode() function.
    These functions rely on that function.

--- lib/src/cookie.dart
+++ lib/src/cookie.dart
@@ -1,5 +1,27 @@
 class CookieStore {
+  /// Regex string that matches an LDH Label. Matches the entire string only.
+  ///
+  /// LDH Label format defined in RFC 5890 Section 2.3.1:
+  ///
+  /// ASCII uppercase, lowercase, or numbers. Dashes allowed other than in the
+  /// first and last position. Complete string must not be longer than
+  /// 63 octets.
+  ///
+  /// More information:
+  ///   https://datatracker.ietf.org/doc/html/rfc5890#section-2.3.1
+  static const String _ldhLabelRegexString =
+      "/\\A[A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9]\\Z/";
+
   List<Cookie> cookies = [];
+
+  (
+    String name,
+    String value,
+    Map<String, String> attrs,
+  ) parseSetCookie(String header) {
+    // TODO: Implement
+    throw UnimplementedError();
+  }
 
   /// Process given Set-Cookie and add to [cookies].
   /// It must be already broken down to name, value, and lowercase attributes
@@ -95,12 +117,59 @@ class CookieStore {
     return true;
   }
 
+  /// Tests whether the two domains are equivalent
   bool _domainCompare(String x, String y) {
-    throw UnimplementedError();
+    return _toCanonical(x) == _toCanonical(y);
   }
 
+  /// Converts a given [requestDomain] to a canonical representation per RFC6265
+  ///
+  /// More information: RFC6265 Section 5.1.2
+  ///             https://datatracker.ietf.org/doc/html/rfc6265#section-5.1.2
   String _toCanonical(String requestDomain) {
-    throw UnimplementedError();
+    var outLabels = [];
+    // Step 1
+    final labels = requestDomain.split('.');
+    // Step 2
+    for (var label in labels) {
+      // A. Check if the label is not an NR-LDH label:
+      //    NR-LDH := LDH && NOT(R-LDH)
+      //            = LDH && NOT(XN-label)
+      //    So NOT(NR-LDH) = NOT(LDH && NOT(XN-label))
+      //                   = NOT(LDH) || XN-label
+
+      /// Is the current [label] not an NR-LDH label?
+      bool notNrLdh = false;
+
+      // First check if it is not an LDH label
+      final ldh = RegExp(_ldhLabelRegexString);
+      notNrLdh = !ldh.hasMatch(label); // If it is a match, it is LDH
+
+      // Then check if it is an XN-label (short circuit if above was true)
+      notNrLdh = notNrLdh || (label[2] == '-' && label[3] == '-');
+
+      // B. If it is not an NR-LDH label, convert it to an A-label
+      //    otherwise, keep it as is
+      if (!notNrLdh) {
+        outLabels.add(label);
+      } else {
+        // An A-label is the sequence "xn--" followed by the output of the
+        // RFC3492 punycode algorithm.
+        outLabels.add("xn--${_toPunyCode(label)}");
+      }
+    }
+    // Step 3
+    // 0x2E is just '.' but the RFC refers to it by codepoint so I figured
+    // I would too
+    return outLabels.join("\x2E");
+  }
+
+  /// Runs the RFC 3492 Punycode algorithm on a given [input] string and returns
+  /// the result.
+  ///
+  /// More information: RFC 3492 https://datatracker.ietf.org/doc/html/rfc3492
+  String _toPunyCode(String input) {
+    throw UnimplementedError("TODO");
   }
 }
 
