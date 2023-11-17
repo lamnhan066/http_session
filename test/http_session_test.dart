c589fb73e57f7a90a6ce174a2b0b9975866eeb40
Ege Feyzioglu <egefeyzioglu@yahoo.com>
2023-11-17 17:06:38 -0500

    Add more tests
    - Test that we send the correct verbs
    - Test that we're not sending cookies to the wrong domains

--- test/http_session_test.dart
+++ test/http_session_test.dart
@@ -1,19 +1,24 @@
 import 'package:http_session/http_session.dart';
 import 'package:test/test.dart';
+
+import 'dart:convert';
 import 'dart:io';
 
 const int testPort = 8888;
 const testURL = 'http://localhost:$testPort';
 
+late HttpServer testServer;
+
+const int serverShutdownTimeLimitSeconds = 15;
+
 void main() async {
-  late HttpServer testServer;
   late HttpSession session;
-  setUp(() async {
+  setUpAll(() async {
     // Session object
     session = HttpSession();
     // Test server
     testServer = await HttpServer.bind(InternetAddress.anyIPv4, testPort);
-    testServer.listen((HttpRequest request) {
+    testServer.listen((HttpRequest request) async {
       var response = request.response;
       var uri = request.uri;
       if (uri.path == "/gimmecookies") {
@@ -46,15 +51,30 @@ void main() async {
         // /longRedirectLoop2
         response.statusCode = 302;
         response.headers.add("Location", "/longRedirectLoop1");
+      } else if (uri.path == "/httpdetails") {
+        String content = await utf8.decodeStream(request);
+        response.writeln(jsonEncode(
+            {'method': request.method, 'path': uri.path, 'body': content}));
       } else {
         response.statusCode = 404;
         response.reasonPhrase = "Not Found";
       }
-      response.flush();
+      await response.flush();
       response.close();
     });
   });
-  test('Http Session', () async {
+  tearDownAll(
+    () async {
+      try {
+        testServer
+            .close()
+            .timeout(Duration(seconds: serverShutdownTimeLimitSeconds));
+      } catch (e) {
+        testServer.close(force: true);
+      }
+    },
+  );
+  test('Test that we can save cookies', () async {
     ///TODO: Minimal test, expand
     session.clear();
     expect(session.cookieStore.cookies.length, 0);
@@ -64,5 +84,50 @@ void main() async {
     expect(session.cookieStore.cookies.length, 6);
     session.clear();
     expect(session.cookieStore.cookies.length, 0);
+  });
+  test("Test that we don't send cookies when we shouldn't", () {
+    session.clear();
+    session.get(Uri.parse("$testURL/gimmecookies?name=foo&value=bar"));
+    expect([], session.cookieStore.getCookiesForRequest(testURL, "/somethingelse"));
+  });
+  group('Test HTTP requests being sent correctly -', () {
+    test('GET', () async {
+      Response response = await session.get(Uri.parse("$testURL/httpdetails"));
+      String str = response.body;
+      Object respObj = jsonDecode(str);
+      expect(respObj, {'method': 'GET', 'path': "/httpdetails", 'body': ''});
+    });
+    test('POST', () async {
+      Response response = await session.post(Uri.parse("$testURL/httpdetails"));
+      String str = response.body;
+      Object respObj = jsonDecode(str);
+      expect(respObj, {'method': 'POST', 'path': "/httpdetails", 'body': ''});
+    });
+    test('DELETE', () async {
+      Response response =
+          await session.delete(Uri.parse("$testURL/httpdetails"));
+      String str = response.body;
+      Object respObj = jsonDecode(str);
+      expect(respObj, {'method': 'DELETE', 'path': "/httpdetails", 'body': ''});
+    });
+    test('PATCH', () async {
+      Response response =
+          await session.patch(Uri.parse("$testURL/httpdetails"));
+      String str = response.body;
+      Object respObj = jsonDecode(str);
+      expect(respObj, {'method': 'PATCH', 'path': "/httpdetails", 'body': ''});
+    });
+    test('PUT', () async {
+      Response response = await session.put(Uri.parse("$testURL/httpdetails"));
+      String str = response.body;
+      Object respObj = jsonDecode(str);
+      expect(respObj, {'method': 'PUT', 'path': "/httpdetails", 'body': ''});
+    });
+    test('HEAD', () async {
+      Response response = await session.head(Uri.parse("$testURL/httpdetails"));
+      String str = response.body;
+      Object respObj = jsonDecode(str);
+      expect(respObj, {'method': 'HEAD', 'path': "/httpdetails", 'body': ''});
+    }, skip: "Test server doesn't currently support HEAD requests");
   });
 }
